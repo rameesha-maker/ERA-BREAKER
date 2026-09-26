8268b51b64f5641b19d3be9fa079f258d552b2e4
rameesha-maker <rameesha.yasmeen0@gmail.com>
2026-09-26 15:03:59 +0500

    Add files via upload

--- pause_screen.py
+++ pause_screen.py
@@ -1,18 +1,16 @@
 import pygame
+from settings import(SCREEN_WIDTH,SCREEN_HEIGHT)
 class PauseScreen:
-    def __init__(self,width,height):
-        self.width = width
-        self.height = height
-        self.title_font = pygame.font.Font(None,64)
-        self.font = pygame.font.Font(None,30)
+    def __init__(self):
+        self.title_font = pygame.font.SysFont("arial",55,bold = True)
+        self.font = pygame.font.SysFont("arial",28)
     def draw(self,screen):
-        overlay = pygame.Surface((self.width,self.height),pygame.SRCALPHA)
+        overlay = pygame.Surface((SCREEN_WIDTH,SCREEN_HEIGHT),pygame.SRCALPHA)
         overlay.fill((0,0,0,170))
         screen.blit(overlay,(0,0))
-        title=self.title_font.render("PAUSED",True,(255,255,255))
-        title_rect = title.get_rect(center = (self.width//2,230))
-        screen.blit(title,title_rect)
+        title=self.title_font.render("GAME PAUSED",True,(255,255,255))
+        screen.blit(title,title.get_rect(center = (SCREEN_WIDTH//2,230)))
         text =self.font.render("Press ESC to continue",True,(220,220,220))
-        text_rect = text.get_rect(center=(self.width//2,320))
-        screen.blit(text,text_rect)
+        screen.blit(text,text.get_rect(center = (SCREEN_WIDTH//2,320)))
+        
         pygame.display.flip()
